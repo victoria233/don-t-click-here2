d29baf9b94839719437e82c16b2893cffe53c0cc
Victoria <110432991+victoria233@users.noreply.github.com>
2022-08-10 19:03:29 -0300

    Weather and Confirmation alert

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function welcome(conf) {
+    if (conf === 'yes') {
+        return true;
+    } else {
+        return confirm('💔💔💔 Adios :c');
+    }
+}
+
+let conf = prompt('Esta usted seguro de querer ingresar a este kawaii sitio? 🌺🌺🌺', 'yes');
+
+if (welcome(conf)) {
+    alert('❥ Welcome ^^❣❣');
+} else {
+    alert('Access denied :c');
+}
+
+
 function redireccion() {
     location.href = "https://www.youtube.com/watch?v=uHKfrz65KSU"
 }
